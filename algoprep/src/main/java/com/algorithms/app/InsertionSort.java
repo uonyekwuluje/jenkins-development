fa225aef09515d9dd98227bbc3958adf85a31934
uonyekwuluje <uionyekwuluje@gmail.com>
2020-11-13 10:10:56 -0500

    updating insertion sort

--- algoprep/src/main/java/com/algorithms/app/InsertionSort.java
+++ algoprep/src/main/java/com/algorithms/app/InsertionSort.java
@@ -1,9 +1,28 @@
 package com.algorithms.app;
+import java.util.*;
+import java.util.concurrent.TimeUnit;
 
-public class InsertionSort {
-    public void sort() {
-        System.out.println("Welcome to insertion sort");
-        System.out.println("--------------------------");
-        System.out.println("\n\n");
+public class InsertionSort{
+   public void sort(){
+      try {
+       int[] nums = {6,5,3,11,8,7,2,1,9,4,13,12};
+       //int[] nums = {6,5,3,1,8,7,2,4};
+       System.out.println("Unsorted Array => "+Arrays.toString(nums));
+
+       for (int x = 1; x<= nums.length - 1; x++){
+       int curr = nums[x];
+          for (int y = x; y> 0; y--){
+              TimeUnit.SECONDS.sleep(1); 
+              if (nums[y-1] > nums[y]){
+                 nums[y] = nums[y-1];
+                 nums[y-1] = curr;
+              }
+          }
+          System.out.println("Sorted Array Pass ["+x+"] => "+Arrays.toString(nums));
+       }
+       System.out.println("Sorted Array => "+Arrays.toString(nums));
+    } catch (InterruptedException e) {
+           System.err.format("IOException: %s%n", e);
     }
+   }
 }
